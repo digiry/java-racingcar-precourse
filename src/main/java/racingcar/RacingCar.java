8e1f315ec96dfd46bb0a8871b014f950de653e9f
Kyujin Kang <digiry@gmail.com>
2022-04-25 01:34:39 +0900

    feat(RacingCar): add moveOrStop method with random number

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,10 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar implements Comparable<RacingCar> {
     private final CarName name;
     private final Distance distance;
+    private final RandomUtil randomUtil;
 
     public RacingCar(CarName car_name) {
+        randomUtil = new NextstepRandomUtil();
+        name = new CarName(car_name.toString());
+        distance = new Distance();
+    }
+
+    public RacingCar(CarName car_name, RandomUtil util) {
+        randomUtil = util;
         name = new CarName(car_name.toString());
         distance = new Distance();
     }
@@ -21,8 +31,33 @@ public class RacingCar implements Comparable<RacingCar> {
         distance.increase();
     }
 
+    public void moveOrStop() {
+        if (canMove()) {
+            move();
+        }
+    }
+
+    private boolean canMove() {
+        int randomValue = randomUtil.getNumberInRange(0, 9);
+
+        return randomValue > 3;
+    }
+
     @Override
     public int compareTo(RacingCar other) {
         return distance.compareTo(other.getDistance());
     }
+
+    public interface RandomUtil {
+        int getNumberInRange(final int startInclusive, final int endInclusive);
+    }
+
+    public static class NextstepRandomUtil implements RandomUtil {
+
+        @Override
+        public int getNumberInRange(final int startInclusive, final int endInclusive) {
+            return Randoms.pickNumberInRange(startInclusive, endInclusive);
+        }
+    }
+
 }
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.RacingCar.RandomUtil;
 
 public class RacingCarTest {
 
@@ -28,5 +29,50 @@ public class RacingCarTest {
         int result = a_car.compareTo(b_car);
 
         assertThat(result).isEqualTo(expected);
+    }
+
+    class FakeRandomUtil implements RandomUtil {
+        Integer testNumber;
+
+        FakeRandomUtil(Integer number) {
+            testNumber = number;
+        }
+
+        @Override
+        public int getNumberInRange(int startInclusive, int endInclusive) {
+            return testNumber;
+        }
+    }
+
+    RacingCar makeRacingCarWithRandomValue(String car_name, int number) {
+        RandomUtil fakeRandomUtil = new FakeRandomUtil(number);
+
+        RacingCar test_car = new RacingCar(new CarName(car_name), fakeRandomUtil);
+
+        return test_car;
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4:1", "7:1", "9:1"}, delimiter = ':')
+    @DisplayName("4이상 랜덤값에서 경주 자동차가 한 칸 이동하는지 검사한다")
+    void moveOneStepIfRandomIsGreaterEqualThan4(int random, int expectedDistance) {
+        RacingCar test_car = makeRacingCarWithRandomValue("a_car", random);
+
+        test_car.moveOrStop();
+        Distance test_car_dist = test_car.getDistance();
+
+        assertThat(test_car_dist.getNumericDistance()).isEqualTo(expectedDistance);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"3:0", "1:0", "0:0"}, delimiter = ':')
+    @DisplayName("3이하 랜던값에서 경주 자동차가 정지하는지 검사한다")
+    void stopRacingCarIfRandomIsLessEqualThan3(int random, int expectedDistance) {
+        RacingCar test_car = makeRacingCarWithRandomValue("a_car", random);
+
+        test_car.moveOrStop();
+        Distance test_car_dist = test_car.getDistance();
+
+        assertThat(test_car_dist.getNumericDistance()).isEqualTo(expectedDistance);
     }
 }
