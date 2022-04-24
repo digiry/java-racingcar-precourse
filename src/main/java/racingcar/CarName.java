db3e9fb3ad250f87ead6c4752f9c99d38069617c
Kyujin Kang <digiry@gmail.com>
2022-04-24 15:07:36 +0900

    feat(CarName): check car naming rule

--- src/main/java/racingcar/CarName.java
+++ src/main/java/racingcar/CarName.java
@@ -2,8 +2,12 @@ package racingcar;
 
 public class CarName {
     private String name;
+    private final int MAX_NAME_SIZE = 5;
 
     public CarName(String car_name) {
+        if (car_name.length() > MAX_NAME_SIZE) {
+            throw new IllegalArgumentException(String.format("이름 길이 5자 규칙 초과 - 입력 길이: ", car_name.length()));
+        }
         name = car_name;
     }
 
--- src/test/java/racingcar/CarNameTest.java
+++ src/test/java/racingcar/CarNameTest.java
@@ -1,8 +1,8 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +14,25 @@ public class CarNameTest {
         CarName test_car = new CarName(simple_name);
 
         assertThat(test_car).hasToString(simple_name);
+    }
+
+    @Test
+    @DisplayName("5자 이하 이름 규칙에 맞는 이름으로 자동자 이름을 생성하는지 검사한다")
+    void createCarNameWithin5LettersName() {
+        String fiveLetters = "abcde";
+
+        CarName test_car = new CarName(fiveLetters);
+
+        assertThat(test_car).hasToString(fiveLetters);
+    }
+
+    @Test
+    @DisplayName("6자 이상으로 이름 규칙에 벗어난 이름으로 자동차 이름을 생성할때 예외를 발생하는지 검사한다")
+    void throwIllegalArgumentExceptionIfCarNameWith6lettersMore() {
+        String sixLetters = "abcdef";
+
+        assertThatThrownBy(() -> {
+            CarName test_car = new CarName(sixLetters);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
